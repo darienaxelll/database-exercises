114d5011ba97498ab880b8123815b341f5a782c2
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-25 14:24:53 -0500

    Added some records to alnbum table

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_name VARCHAR(50),
     album_name VARCHAR(50),
-    relese_date INT,
+    release_date INT,
     sales INT,
     genre VARCHAR(50),
     PRIMARY KEY(id)
---
+++ albums_seeder.sql
@@ -0,0 +1,5 @@
+INSERT INTO albums (album_name, artist_name, release_date, sales, genre)
+VALUES ('Thiller', 'Micheal Jackson', 1982,70,'Pop/Funk'),
+       ('AC/DC', 'Black in Black', 1980,50,'Hard Rock'),
+       ('Whitneey Houston', 'The Bodyguard', 1992, 45,'R&B'),
+       ('Pink Floyd', 'The Dark Side of the Moon', 1973,45, 'Progressive Rock')
