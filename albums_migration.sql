e581b65d04c9bf9777a738ace48190eca9d5a840
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-25 11:26:53 -0500

    Created artist table

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS quotes;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist_name VARCHAR(50),
+    album_name VARCHAR(50),
+    relese_date INT,
+    sales INT,
+    genre VARCHAR(50),
+    PRIMARY KEY(id)
+);
